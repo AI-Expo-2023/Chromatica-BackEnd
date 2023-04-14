6422d8103c78df7e407ac78a17fe42656e500039
XII <ur12xii@gmail.com>
2023-04-14 14:16:28 +0900

    :hammer: updated saveImage(first)

--- src/controller/design.js
+++ src/controller/design.js
@@ -1,43 +1,34 @@
 const { Save } = require('../models');
-const path = require('path')
 
 require('dotenv').config();
 
-const saveImage = async (req, res) => {
+const createSaveImage = async (req, res) => {
     const userID = req.decoded.id;
-    const accessToken = req.headers.authorization;
 
-    const ext = path.extname(req.file.originalname);
-    const filePath = `${path.basename(req.file.originalname, ext)}${Date.now()}${ext}`;
+    const photo = req.body.imageURL;
+    console.log(photo, req.body)
 
     try {
-        if (!accessToken) {
+        if (!userID) {
             return res.status(401).json({
                 "message" : "로그인이 필요합니다." 
             })
         }
-
         const thisSave = await Save.findOne({
-            where: {userID},
+            where: { photo }
         })
-
-        if (!thisSave) {
-            const newSave = await Save.create({
-                userID,
-                photo: filePath,
-            })
-
-            return res.status(201).json({
-                "photoPath" : newSave.photo
+        if (thisSave) {
+            return res.status(409).json({
+                "message" : "중복된 저장입니다."
             })
         }
-
-        await thisSave.update({
-            photo : filePath
+        const newSave = await Save.create({
+            userID,
+            photo,
         })
-
         return res.status(201).json({
-            "photoPath": thisSave.photo
+            "message" : "요청에 성공했습니다.",
+            "imageURL" : newSave.photo
         })
     } catch (err) {
         console.error(err)
@@ -47,4 +38,6 @@ const saveImage = async (req, res) => {
     }
 }
 
-module.exports = { saveImage }
+module.exports = {
+    createSaveImage,
+}
