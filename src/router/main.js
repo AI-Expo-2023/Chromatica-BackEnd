8bd8f47dbfb8be7a5d3f341cd643edb999e6b657
XII <ur12xii@gmail.com>
2023-04-18 17:22:51 +0900

    :hammer: updated getMain func

--- src/router/main.js
+++ src/router/main.js
@@ -2,14 +2,6 @@ const router = require('express')();
 
 const controller = require('../controller/main');
 
-const { Photo } = require('../models');
-
-setInterval(async () => {
-    await Photo.findAll({
-        order: [['like', 'DESC']],
-    })
-}, 1000 * 3600 * 24)
-
 router.get('/', controller.getMain);
 
 module.exports = router;
