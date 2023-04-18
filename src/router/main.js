d6b2efac8baf2636fa88158f3f1f7707e15932d1
XII <ur12xii@gmail.com>
2023-04-18 23:06:24 +0900

    :sparkles: :: created getLastPhoto func

--- src/router/main.js
+++ src/router/main.js
@@ -4,5 +4,6 @@ const controller = require('../controller/main');
 
 router.get('/', controller.getMain);
 router.get('/rank', controller.getRank);
+router.get('/gallery', controller.getLastPhoto);
 
 module.exports = router;
